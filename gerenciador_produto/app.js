73a9de2ffc36fdcc7ef2d3341d51c572e01bdbcc
CAROLINE ROCHA RODRIGUES <caroline.rodrigues117@etec.sp.gov.br>
2025-03-14 10:05:52 -0300

    FUncionougit add -A

---
+++ gerenciador_produto/app.js
@@ -0,0 +1,8 @@
+//arquivo principal que importa e executa as funcoes
+import produtos from './produtos.js';//importa colecao inteira(todas as funcoes desse arquivo js)
+//. indica pasta atual e / significa dentro da pasta
+
+//mostra no terminal a funcao chamando a partir da colecao
+console.log(produtos.mapear());
+console.log(produtos.filtrar());//colecao.nomedafuncao
+console.log(produtos.reduzir());//parenteses por ser funcao!
---
+++ gerenciador_produto/produtos.js
@@ -0,0 +1,34 @@
+//contem os produtos e funcoes de manipulacao
+
+const produtos = [
+    { "id": 1, "nome": "Caderno Universitário 200 folhas", "preco": 25.90 },
+    { "id": 2, "nome": "Caneta Esferográfica Azul", "preco": 2.50 },
+    { "id": 3, "nome": "Lápis Preto HB", "preco": 1.80 },
+    { "id": 4, "nome": "Borracha Branca", "preco": 3.00 },
+    { "id": 5, "nome": "Marcador de Texto Amarelo", "preco": 6.90 },
+    { "id": 6, "nome": "Estojo Escolar", "preco": 29.90 },
+    { "id": 7, "nome": "Bloco de Notas Adesivas", "preco": 12.50 },
+    { "id": 8, "nome": "Grampeador Pequeno", "preco": 19.90 },
+    { "id": 9, "nome": "Régua 30cm Transparente", "preco": 7.50 },
+    { "id": 10, "nome": "Agenda 2025 Capa Dura", "preco": 39.90 }
+]
+
+function mapear(){
+    return produtos.map(({nome}) => (nome)); //funcao nomeada retornando arrow function (funcao anonima reduzida)
+}
+
+function filtrar(){
+    return produtos.filter(({preco}) => preco == "29.90"); //filer vai comparar o preço com o parametro de 29.90 dado
+}
+
+function reduzir(){//tem varios propositos: somar todos os numeros, achar o maior elemento e outros
+    produtos.reduce((acc,{preco}) => acc + (preco), 0); //valor inicial sempre será zero
+    // acc é acumulador e curr o valor atual (preco nesse caso)
+}
+
+export default { //faz coleção com todas funcoes (default importa tudo como se fosse uma unica coisa)
+    mapear,
+    filtrar,
+    reduzir
+}
+
